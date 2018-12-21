f731cc4442b83db07b6a48156afc3dca35209c19
adamcweiner <adamcweiner@gmail.com>
2018-12-20 17:23:35 -0800

    add larger sequencing files

--- fluv/run_validation.py
+++ fluv/run_validation.py
@@ -2,3 +2,4 @@ from validation import historical_validation
 
 V = historical_validation()
 V.calc_PAM()
+V.calc_FLU()
--- fluv/validation.py
+++ fluv/validation.py
@@ -12,14 +12,26 @@ class historical_validation:
         # TODO: add array of measured vaccine efficacies... make sure indexing matches sequence ordering
         assert(len(self.vax_seqs) == len(self.circ_seqs))
         self.size = len(self.vax_seqs)
-        
+
     def calc_PAM(self):
-        """ Caclulate distances between circulating strain and vaccine in each given year. """
+        """ Caclulate distances between circulating strain and vaccine in each given year. Using PAM250 matrix here. """
         D_PAM = Distance("PAM250")
-        
+
         distances = np.zeros((self.size))
         for ii in range(self.size):
             distances[ii] = D_PAM.seq_dist(self.vax_seqs[ii], self.circ_seqs[ii])
+        
+        print(distances)
+        return distances # this is temporary
+        # add plot where x-axis is `self.eff` and y-axis is `distances`... maybe use `circ_labels` as legend
+
+    def calc_FLU(self):
+        """ Caclulate distances between circulating strain and vaccine in each given year. Using FLU sub. matrix here."""
+        D_FLU = Distance("FLU")
+
+        distances = np.zeros((self.size))
+        for ii in range(self.size):
+            distances[ii] = D_FLU.seq_dist(self.vax_seqs[ii], self.circ_seqs[ii])
         
         print(distances)
         return distances # this is temporary
