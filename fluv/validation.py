1b05ba1a9d3651b4cfe96a72f76006bcbf2f0c14
adamcweiner <adamcweiner@gmail.com>
2019-05-02 10:40:38 -0700

    move to data folder

--- fluv/validation.py
+++ fluv/validation.py
@@ -9,10 +9,10 @@ class historical_validation:
     def __init__(self):
         """ Load sequences and vaccine efficacy scores according to literature. """
         print("class being initialized")
-        _, _ = trim("NA_2012_aligned_seq.fa") # test that sequence_processing still works
+        _, _ = trim("data/NA_2012_aligned_seq.fa") # test that sequence_processing still works
         print("done trimming NA_2012 sequences")
-        self.circ_labels, self.circ_seqs = trim("aligned_circulating_strains.fa")
-        self.vax_labels, self.vax_seqs = trim("aligned_vaccine_strains.fa")
+        self.circ_labels, self.circ_seqs = trim("data/aligned_circulating_strains.fa")
+        self.vax_labels, self.vax_seqs = trim("data/aligned_vaccine_strains.fa")
         self.year = np.array([1971, 1972, 1973, 1975, 1984, 1985, 1987, 1989, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2001, 2003])
         self.eff = np.array([7, 15, 11, -3, -6, -2, 17, -5, 59, 38, 25, 45, 28, -17, 34, 43, 55, 12]) # https://doi.org/10.1016/j.vaccine.2006.01.010
         assert(len(self.vax_seqs) == len(self.circ_seqs) == len(self.year) == len(self.eff))
