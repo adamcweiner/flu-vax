3d3eb822a91f0626c4370a9826bf43f71e65a416
adamcweiner <adamcweiner@gmail.com>
2019-05-06 10:58:38 -0700

    minor edits

--- fluv/run_vis.py
+++ fluv/run_vis.py
@@ -1,5 +1,5 @@
 """ Execute this file to generate all the embedding plots from 2012 North America sequences. """
-
+import time
 from visualization import generate_dist_mat, generate_MDS, generate_tSNE
 
 methods = ["PAM250_hybrid", "PAM250", "FLU_hybrid", "FLU", "Hamming"]
@@ -7,6 +7,8 @@ for method in methods:
     print("generating dist_mat for", method)
     dist_mat = generate_dist_mat(method)
     print("generating MDS plot")
-    generate_MDS(dist_mat, method, "data/MDS_" + str(method))
+    generate_MDS(dist_mat, method, "Figures/MDS_" + str(method))
     print("generating tSNE plot")
-    generate_tSNE(dist_mat, method, "data/tSNE_" + str(method))
+    generate_tSNE(dist_mat, method, "Figures/tSNE_" + str(method))
+    time_passed = (time.process_time())/60
+    print('{} plots generated in {:.3} minutes'.format(time_passed))
