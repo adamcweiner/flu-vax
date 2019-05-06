14046f36b073e612f6d88bcd5a55fd3b2fe6d64d
adamcweiner <adamcweiner@gmail.com>
2019-05-06 10:52:54 -0700

    finish writing script

--- fluv/run_vis.py
+++ fluv/run_vis.py
@@ -4,5 +4,9 @@ from visualization import generate_dist_mat, generate_MDS, generate_tSNE
 
 methods = ["PAM250_hybrid", "PAM250", "FLU_hybrid", "FLU", "Hamming"]
 for method in methods:
+    print("generating dist_mat for", method)
     dist_mat = generate_dist_mat(method)
-    
+    print("generating MDS plot")
+    generate_MDS(dist_mat, method, "data/MDS_" + str(method))
+    print("generating tSNE plot")
+    generate_tSNE(dist_mat, method, "data/tSNE_" + str(method))
